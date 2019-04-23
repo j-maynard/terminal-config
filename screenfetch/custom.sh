f61581ace48efa9607d44755207f3e6dcff2aa47
Jamie Maynard <james.maynard@digital.cabinet-office.gov.uk>
2019-04-23 11:26:13 +0100

    Externalised the MOTD to it own file

--- screenfetch/custom.sh
+++ screenfetch/custom.sh
@@ -3,8 +3,8 @@
 custom0=$(echo -e "$labelcolor JAVA:$textcolor  ☕️  $(java --version | head -n 1)"); out_array=( "${out_array[@]}" "$custom0" ); ((display_index++));
 custom1=$(echo -e "       $textcolor Use 'java8' or 'java11' to set the java version"); out_array=( "${out_array[@]}" "$custom1" ); ((display_index++));
 custom2=$(echo -e ""); out_array=( "${out_array[@]}" "$custom2" ); ((display_index++));
-custom3=$(echo -e "$labelcolor MOTD:$textcolor Do fairies have tails?"); out_array=( "${out_array[@]}" "$custom3" ); ((display_index++));
-custom4=$(echo -e "$labelcolor      $textcolor More than that, do fairies even exist?"); out_array=( "${out_array[@]}" "$custom4" ); ((display_index++));
-custom5=$(echo -e "$labelcolor      $textcolor Nobody knows for sure.  So this guild is like them,"); out_array=( "${out_array[@]}" "$custom5" ); ((display_index++));
-custom6=$(echo -e "$labelcolor      $textcolor an eternal mystery, an eternal adventure."); out_array=( "${out_array[@]}" "$custom6" ); ((display_index++));
-
+label="MOTD:"
+while read line; do
+  customn=$(echo -e "$labelcolor $label$textcolor $line"); out_array=( "${out_array[@]}" "$customn" ); ((display_index++));
+  label="     "
+done <~/.screenfetch/motd.txt
