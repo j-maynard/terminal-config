d6296795a6e62f1fcd303c69736c42912553a7d4
Jamie Maynard <jamie.maynard@me.com>
2020-06-06 16:50:54 +0100

    Isolated all the Java stuff to one place

--- zsh.d/06-java.zsh
+++ zsh.d/06-java.zsh
@@ -1,5 +1,28 @@
 #!/bin/zsh
 which java > /dev/null 2>&1
-if [[ $? -eq 0 ]]; then
-    java11
+if [[ $? > 0 ]]; then
+    return
 fi
+
+case $(uname) in
+
+  Darwin)       # Set up multiple Java installs on Mac OS
+                export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
+                export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
+                alias java8='export JAVA_HOME=$JAVA_8_HOME'
+                alias java11='export JAVA_HOME=$JAVA_11_HOME'
+                ;;
+                
+  Linux)        # Setup jEnv on linux
+                export PATH="$HOME/.jenv/bin:$PATH"
+                eval "$(jenv init -)"
+                alias java8='jenv global 1.8'
+                alias java11='jenv global 11.0'
+                ;;
+                
+  *)            echo "Unknwon Environment"
+                ;;
+                
+esac
+
+java11
