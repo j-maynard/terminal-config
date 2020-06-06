93e49754f8a7ea72fc802f45c5206be4d23ada06
Jamie Maynard <jamie.maynard@me.com>
2020-06-06 21:19:47 +0100

    Updated the Linux Env script to be the Shim Script
    OK I have various Shim setups including Java and Ruby at the moment.  This script sets up those Shim systems.  Also renamed the script to better reflect what it does and made it follow the same patter as the other scripts.

---
+++ shim-setup.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+STARTPWD=$(pwd)
+
+SCRIPT=`realpath -s $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+if [ -z $GIT_REPO ]; then
+    GIT_REPO="https://raw.githubusercontent.com/j-maynard/terminal-config/master"
+fi
+
+# Define colors and styles
+normal="\033[0m"
+bold="\033[1m"
+green="\e[32m"
+red="\e[31m"
+yellow="\e[93m"
+
+set_username() {
+    if [ -z $SUDO_USER ]; then
+        USERNAME=$USER
+    else
+        USERNAME=$SUDO_USER
+    fi
+    if [ $USERNAME == "root" ]; then
+        USER_PATH="/root"
+    else
+        USER_PATH="/home/$USER"
+    fi
+}
+
+usage() {
+    echo -e "Usage:"
+    echo -e "  ${bold}${red}-V  --verbose${normal}                Shows command output for debugging"
+    echo -e "  ${bold}${red}-v  --version${normal}                Shows version details and exit"
+    echo -e "  ${bold}${red}-h  --help${normal}                   Shows this usage message and exit"
+}
+
+version() {
+    echo -e "Shim Setup Script Version 0.5"
+    echo -e "(c) Jamie Maynard 2020"
+}
+
+show_msg() {
+    echo -e $1 > /dev/tty
+}
+
+install_rbenv() {
+  if [ ! -d "${USER_PATH}/.rbenv" ]; then
+    show_msg "Setting RBEnv up...."
+    git clone $GIT_QUIET https://github.com/rbenv/rbenv.git $USER_PATH/.rbenv
+    cd $USER_PATH/.rbenv && src/configure && make -C src
+    mkdir -p $USER_PATH/.rbenv/plugins
+    git clone -q https://github.com/rbenv/ruby-build.git $USER_PATH/.rbenv/plugins/ruby-build
+  fi
+}
+
+install_jenv() {
+  if [[ $(uname -a) != "Linux" ]]; then
+    return
+  fi
+  
+  if [ ! -d "${USER_PATH}/.jenv" ]; then
+    show_msg "Setting JEnv up...."
+    git clone $GIT_QUIET https://github.com/gcuisinier/jenv.git $USER_PATH/.jenv
+    mkdir $USER_PATH/.jenv/versions
+    $USER_PATH/.jenv/bin/jenv add /usr/lib/jvm/java-11-openjdk-amd64
+    $USER_PATH/.jenv/bin/jenv add /usr/lib/jvm/java-8-openjdk-amd64
+  fi
+}
+
+################################
+# Main Script body starts here #
+################################
+
+# Set default options
+VERBOSE=false
+
+# Process commandline arguments
+while [ "$1" != "" ]; do
+    case $1 in
+        V | -V | --verbose)           VERBOSE=true
+					                            VARG="-V"
+                                    	;;
+        v | -v | --version)           version
+                                    	exit 0
+                                    	;;
+        h | -h | --help)              usage
+                                    	exit 0
+                                    	;;
+        * )                         	echo -e "Unknown option $1...\n"
+                                    	usage
+                                    	exit 1
+                                    	;;
+    esac
+    shift
+done
+
+# Silence output
+if [ $VERBOSE == "false" ]; then
+    echo "Silencing output"
+    exec > /dev/null
+    GIT_QUIET="-q"
+fi
+
+install_rbenv
+install_jenv
+
