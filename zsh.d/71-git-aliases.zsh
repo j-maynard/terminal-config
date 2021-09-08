13669f0604375a2f4505f51f471949fd1b008498
Jamie Maynard <jamie.maynard@digital.cabinet-office.gov.uk>
2021-09-08 10:09:35 +0100

    Updates to gitconfig and git-aliases
    A few updates to make my git life a little easier

--- zsh.d/71-git-aliases.zsh
+++ zsh.d/71-git-aliases.zsh
@@ -21,4 +21,11 @@ if [[ $? -eq 0 ]]; then
     alias force-push='git commit --amend --no-edit && git push -f'
     alias fp='force-push'
     alias push-new="git push --set-upstream origin \$(git branch | grep '*' | cut -d ' ' -f 2)"
+    if [[ $(uname) == "Darwin" ]]; then
+        alias checkout-master-all='find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} checkout master'
+        alias pull-all='find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull origin master \;'
+    else
+        alias checkout-master-all='find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} checkout master \;'
+        alias pull-all='find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} pull \;'
+    fi
 fi
